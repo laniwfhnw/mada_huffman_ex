7521c84a25fd48a1d1c70c643a48ebcb03bf3836
lanijw <107607489+lanijw@users.noreply.github.com>
2022-12-07 21:00:43 +0100

    Moves command util functions to own class.

--- src/main/ch/fhnw/huffman/commands/DecodeCommand.java
+++ src/main/ch/fhnw/huffman/commands/DecodeCommand.java
@@ -1,17 +1,15 @@
 package ch.fhnw.huffman.commands;
 
-import ch.fhnw.huffman.FileUtil;
 import ch.fhnw.huffman.HuffmanEncoding;
 
-import java.io.File;
-import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.util.Scanner;
 
-import static ch.fhnw.huffman.ConsoleUtil.getUserInput;
 import static ch.fhnw.huffman.FileUtil.readBytes;
 import static ch.fhnw.huffman.FileUtil.readString;
 import static ch.fhnw.huffman.FileUtil.write;
+import static ch.fhnw.huffman.commands.CommandUtil.askForExistingPath;
+import static ch.fhnw.huffman.commands.CommandUtil.askForOutputPath;
 
 public class DecodeCommand implements Command {
   @Override public boolean execute(Scanner s) {
@@ -28,49 +26,9 @@ public class DecodeCommand implements Command {
     bitSeq.delete(bitSeq.lastIndexOf("1"), bitSeq.length());
 
     write(outputPath,
-          HuffmanEncoding.fromEncoding(readString(encodingSchemePath), bitSeq.toString())
+          HuffmanEncoding.fromEncoding(readString(encodingSchemePath),
+                                       bitSeq.toString())
                          .getPlainText());
     return true;
-  }
-
-  private static Path askForExistingPath(Scanner s, String contentDesc) {
-    Path filePath = Path.of("");
-    boolean userInputValid = false;
-    while (!userInputValid) {
-      System.out.printf(
-          "Where is the %s stored?%n" +
-              "Current working directory is \"%s\".%n> ",
-          contentDesc, FileUtil.getCwd());
-      try {
-        filePath = Path.of(getUserInput(s));
-        userInputValid = true;
-      } catch (InvalidPathException ignored) {
-        System.out.printf("Invalid path %s%n", filePath);
-      }
-      if (userInputValid &&
-              !new File(filePath.toString()).exists()) {
-        userInputValid = false;
-        System.out.printf("File \"%s\" not found.%n", filePath);
-      }
-    }
-    return filePath;
-  }
-
-  private static Path askForOutputPath(Scanner s, String contentDesc) {
-    Path filePath = Path.of("");
-    boolean userInputValid = false;
-    while (!userInputValid) {
-      System.out.printf(
-          "Where should the %s be stored?%n" +
-              "Current working directory is \"%s\".%n> ",
-          contentDesc, FileUtil.getCwd());
-      try {
-        filePath = Path.of(getUserInput(s));
-        userInputValid = true;
-      } catch (InvalidPathException ignored) {
-        System.out.printf("Invalid path %s%n", filePath);
-      }
-    }
-    return filePath;
   }
 }
